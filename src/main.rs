75a498939fc9ccb228715b1a920112fa3766b4a2
Thrigger <21341498+Thrigger@users.noreply.github.com>
2023-09-24 21:42:55 +0200

    Add string token
    - Add string token parsing
    - Move parser to own module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod config;
 mod token;
+mod parser;
 
-use token::{Token, TokenType};
+use token::Token;
 
 use std::fs;
 
@@ -14,7 +15,7 @@ fn main() {
     let contents = fs::read_to_string(args.template).unwrap();
     println!("Content of template:\n{contents:?}");
 
-    let tokens: Vec<Token> = scan(&contents);
+    let tokens: Vec<Token> = parser::scan(&contents);
 
     println!("This is the result:\n");
     println!("-------------");
@@ -25,36 +26,3 @@ fn main() {
 
 }
 
-fn scan(cont: &str) -> Vec<Token> {
-    let mut i = 0;
-    let mut tokens = vec![];
-
-    while i < cont.len() {
-        if let Some(tok) = get_one_char_token(cont, i) {
-            tokens.push(tok);
-            i += 1;
-        } else if let Some((tok, new_i))  = get_keyword_token(cont, i) {
-            tokens.push(tok);
-            i = new_i;
-        } else {
-            i+=1;
-        }
-    }
-    tokens
-}
-
-fn get_one_char_token(s: &str, i: usize) -> Option<Token> {
-    match &s[i..i+1] {
-        "(" => Some(Token::new(TokenType::LeftPara, None)),
-        ")" => Some(Token::new(TokenType::RightPara, None)),
-        _ => None,
-    }
-}
-fn get_keyword_token(s: &str, i: usize) -> Option<(Token, usize)> {
-    if s.len() >= i + 7 && &s[i..i+7] == "println" {
-        return Some((Token::new(TokenType::Println, None), 7));
-    } else if s.len() >= i + 5 && &s[i..i+5] == "print" {
-        return Some((Token::new(TokenType::Print, None), 5));
-    }
-    None
-}
---
+++ src/parser.rs
@@ -0,0 +1,68 @@
+use crate::token::{Token, TokenType};
+
+pub fn scan(cont: &str) -> Vec<Token> {
+    let mut i = 0;
+    let mut tokens = vec![];
+
+    while i < cont.len() {
+        if let Some(tok) = get_one_char_token(cont, i) {
+            tokens.push(tok);
+            i += 1;
+        } else if let Some((tok, new_i))  = get_keyword_token(cont, i) {
+            tokens.push(tok);
+            i = new_i;
+        } else if let Some((tok, new_i))  = get_string_token(cont, i) {
+            tokens.push(tok);
+            i = new_i;
+        } else {
+            i+=1;
+        }
+    }
+    tokens
+}
+
+fn get_string_token(s: &str, i: usize) -> Option<(Token, usize)> {
+    if &s[i..i+1] == "\"" {
+        let string_start = i + 1;
+        if let Some(i_end) = &s[string_start..].find("\"") {
+            return Some(
+                (Token::new(TokenType::Str(String::from(&s[string_start..string_start+i_end])), None),
+                string_start+1+i_end));
+        }
+    }
+    None
+}
+
+fn get_one_char_token(s: &str, i: usize) -> Option<Token> {
+    match &s[i..i+1] {
+        "(" => Some(Token::new(TokenType::LeftPara, None)),
+        ")" => Some(Token::new(TokenType::RightPara, None)),
+        _ => None,
+    }
+}
+
+fn get_keyword_token(s: &str, i: usize) -> Option<(Token, usize)> {
+    if s.len() >= i + 7 && &s[i..i+7] == "println" {
+        return Some((Token::new(TokenType::Println, None), 7));
+    } else if s.len() >= i + 5 && &s[i..i+5] == "print" {
+        return Some((Token::new(TokenType::Print, None), 5));
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ok_get_string_token() {
+        let code = String::from("\"abc\"");
+
+        if let Some((Token, i_new)) = get_string_token(&code, 0) {
+            assert_eq!(4, i_new);
+        } else {
+            panic!("Does not parse correct!");
+        }
+    }
+}
+
